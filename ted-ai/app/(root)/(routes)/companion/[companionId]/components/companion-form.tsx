2fd75c67c9d2300fba126147a7666076b3cf2530
Sanskar <thakursanskaranhal@gmail.com>
2023-11-30 20:57:21 +0530

    made the form

---
+++ ted-ai/app/(root)/(routes)/companion/[companionId]/components/companion-form.tsx
@@ -0,0 +1,91 @@
+"use client"
+
+import * as z from "zod";
+
+import { Category, Companion } from "@prisma/client";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod"
+import { Form, FormControl, FormField, FormItem, FormMessage } from "@/components/ui/form";
+import { Separator } from "@/components/ui/separator";
+
+interface CompanionFormProps {
+    initialData: Companion | null;
+    categories: Category[];
+}
+
+const formSchema = z.object({
+    name: z.string().min(1, {
+        message: "Name is required."
+    }),
+    description: z.string().min(1, {
+        message: "Description is required."
+    }),
+    instructions: z.string().min(200, {
+        message: "Instructions required atleast 200 characters."
+    }),
+    seed: z.string().min(200, {
+        message: "Seed required atleast 200 characters."
+    }),
+    source: z.string().min(1, {
+        message: "Image is required."
+    }),
+    categoryId: z.string().min(1, {
+        message: "Category is required."
+    }),
+
+})
+
+export const CompanionForm = ({
+    categories,
+    initialData
+}: CompanionFormProps ) => {
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: initialData || {
+            name: "",
+            description: "",
+            instructions: "",
+            seed: "",
+            source: "",
+            categoryId: undefined,
+        }
+    })
+
+    const isLoading = form.formState.isSubmitting;
+    
+    const onSubmit =async (values:z.infer<typeof formSchema>) => {
+        console.log(values);
+    }
+
+    return (
+        <div className=" h-full p-4 space-y-2 max-w-3xl mx-auto">
+            <form {...form}>
+                <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 pb-10">
+                    <div className="space-y-2 w-full col-span-2">
+                        <div>
+                            <h3 className="text-lg font-medium">
+                                General Information 
+                            </h3>
+                            <p className=" text-sm text-muted-foreground">
+                                General Information about your companion
+                            </p>
+                        </div>
+                        <Separator className=" bg-primary/10"/>
+                    </div>
+                    <FormField 
+                        name="src"
+                        render={({ field }) => (
+                            <FormItem className="flex flex-col items-center justify-center space-y-4 col-span-2">
+                                <FormControl>
+                                    Image Upload components
+                                </FormControl>
+                                <FormMessage/>
+                            </FormItem>
+                        )}
+                    />
+                </form>
+            </form>
+        </div>
+    )
+}
--- ted-ai/app/(root)/(routes)/companion/[companionId]/page.tsx
+++ ted-ai/app/(root)/(routes)/companion/[companionId]/page.tsx
@@ -1,8 +1,31 @@
-const CompanionIdPage = () => {
+import prismadb from "@/lib/prismadb";
+import { CompanionForm } from "./components/companion-form";
+
+interface CompanionIdPageProps {
+    params: {
+        companionId: string;
+    }
+}
+
+const CompanionIdPage = async ({
+    params
+}: CompanionIdPageProps) => {
+
+    //TODO check for subscription
+
+    const Companion = await prismadb.companion.findUnique({
+        where: {
+            id: params.companionId,
+        }
+    });
+
+    const categories = await prismadb.category.findMany();
+
     return ( 
-        <div>
-            Companion Id
-        </div>
+        <CompanionForm 
+            initialData={Companion}
+            categories={categories}
+        />
      );
 }
  
