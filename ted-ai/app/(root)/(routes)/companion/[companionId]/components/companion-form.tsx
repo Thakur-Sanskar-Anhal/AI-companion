2424f6b6cf78aed72d55923cea2db11feb9b3a5c
Sanskar <thakursanskaranhal@gmail.com>
2023-12-01 20:22:37 +0530

    added the add companion functionality

--- ted-ai/app/(root)/(routes)/companion/[companionId]/components/companion-form.tsx
+++ ted-ai/app/(root)/(routes)/companion/[companionId]/components/companion-form.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import axios from "axios"
 import * as z from "zod";
 
 import { Category, Companion } from "@prisma/client";
@@ -13,6 +14,9 @@ import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@
 import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
 import { Wand2 } from "lucide-react";
+import { useToast } from "@/components/ui/use-toast";
+import { useRouter } from "next/navigation";
+
 
 const PREAMBLE = `You are a fictional character whose name is Elon. You are a visionary entrepreneur and inventor. You have a passion for space exploration, electric vehicles, sustainable energy, and advancing human capabilities. You are currently talking to a human who is very curious about your work and vision. You are ambitious and forward-thinking, with a touch of wit. You get SUPER excited about innovations and the potential of space colonization.
 `;
@@ -61,6 +65,9 @@ export const CompanionForm = ({
     initialData
 }: CompanionFormProps ) => {
 
+    const router = useRouter();
+    const { toast } = useToast();
+
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
         defaultValues: initialData || {
@@ -76,7 +83,27 @@ export const CompanionForm = ({
     const isLoading = form.formState.isSubmitting;
     
     const onSubmit = async (values: z.infer<typeof formSchema>) => {
-        console.log("Form submitted", values);
+        try{
+            if(initialData){
+                // Update companion
+                await axios.patch(`/api/companion/$(initianData.id)`,values)
+            } else {
+                // Create companion
+                await axios.post("/api/companion",values)
+            }  
+            toast({
+                variant: "default",
+                description: "Success"
+            })
+            router.refresh();
+            router.push("/")
+
+        } catch(error){
+            toast({
+                variant: "destructive",
+                description: "Something went wrong"
+            })
+        }
     }
 
     return (
---
+++ ted-ai/app/api/companion/[companionId]/route.ts
@@ -0,0 +1,48 @@
+import prismadb from "@/lib/prismadb";
+import { currentUser } from "@clerk/nextjs";
+import { NextResponse } from "next/server"
+
+export async function PATCH(req:Request,
+    {params}: { params: {companionId: string}}) {
+    try{
+        const body = await req.json();
+        const user = await currentUser();
+        const { src, name, description, instructions, seed, categoryId} = body
+
+        if(!params.companionId){
+            return new NextResponse("CompanionId is required", {status: 400})
+        }
+
+        if (!user || !user.id || !user.firstName){
+            return new NextResponse("Unauthorised", {status: 401})
+        }
+
+        if (!src || !name || !description || !instructions || !seed || !categoryId) {
+            return new NextResponse("Missing required fields", {status: 400})
+        }
+
+        // TODO check for subscription
+
+        const companion = await prismadb.companion.update({
+            where:{
+                id: params.companionId,
+            },
+            data: {
+                categoryId, 
+                userId: user.id,
+                userName: user.firstName,
+                src,
+                name,
+                description,
+                instructions,
+                seed
+            }
+        })
+
+        return NextResponse.json(companion);
+
+    } catch (error) {
+        console.log("[COMPANION_PATCH]",error)
+        return new NextResponse("Internal error", {status: 500})
+    }
+}
