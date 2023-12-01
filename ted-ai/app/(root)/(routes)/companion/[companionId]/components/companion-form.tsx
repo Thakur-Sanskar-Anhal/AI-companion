7500aa60ac62a05f7529045185ac52b23b72d5be
Sanskar <thakursanskaranhal@gmail.com>
2023-12-01 18:34:08 +0530

    added  the genral information form

--- ted-ai/app/(root)/(routes)/companion/[companionId]/components/companion-form.tsx
+++ ted-ai/app/(root)/(routes)/companion/[companionId]/components/companion-form.tsx
@@ -5,9 +5,11 @@ import * as z from "zod";
 import { Category, Companion } from "@prisma/client";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod"
-import { Form, FormControl, FormField, FormItem, FormMessage } from "@/components/ui/form";
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { Separator } from "@/components/ui/separator";
 import { ImageUpload } from "@/components/image-upload";
+import { Input } from "@/components/ui/input";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 
 const formSchema = z.object({
     name: z.string().min(1, {
@@ -88,6 +90,96 @@ export const CompanionForm = ({
                             </FormItem>
                             )}
                         />
+                        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                            <FormField 
+                                name="name"
+                                control={form.control}
+                                render={({ field }) =>(
+                                    <FormItem className=" col-span-2 md:col-span-1">
+                                        <FormLabel>Name</FormLabel>
+                                        <FormControl>
+                                            <Input
+                                                disabled={isLoading}
+                                                placeholder="Elon Musk"
+                                                {...field}  
+                                            />
+                                        </FormControl>
+                                        <FormDescription>
+                                            This is how your ai companion will be named
+                                        </FormDescription>
+                                        <FormMessage/>
+                                    </FormItem>
+                                )}
+                            />
+                            < FormField 
+                                name="description"
+                                control={form.control}
+                                render={({ field }) =>(
+                                    <FormItem className=" col-span-2 md:col-span-1">
+                                        <FormLabel>Description</FormLabel>
+                                        <FormControl>
+                                            <Input
+                                                disabled={isLoading}
+                                                placeholder="CEO and founder of Tesla and Space X"
+                                                {...field}  
+                                            />
+                                        </FormControl>
+                                        <FormDescription>
+                                            Short description for your ai companion
+                                        </FormDescription>
+                                        <FormMessage/>
+                                    </FormItem>
+                                )}
+                            />
+                            < FormField 
+                                name="categoryId"
+                                control={form.control}
+                                render={({ field }) =>(
+                                    <FormItem>
+                                        <FormLabel>Category</FormLabel>
+                                        <Select
+                                            disabled={isLoading}
+                                            onValueChange={field.onChange}
+                                            value={field.value}
+                                            defaultValue={field.value}
+                                        >
+                                            <FormControl>
+                                                <SelectTrigger className=" bg-background">
+                                                    <SelectValue 
+                                                        defaultValue={field.value}
+                                                        placeholder="Select a gategory"
+                                                    />
+                                                </SelectTrigger>
+                                            </FormControl>   
+                                            <SelectContent>
+                                                {categories.map((category) =>(
+                                                    <SelectItem
+                                                        key={category.id}
+                                                        value={category.id}
+                                                    >
+                                                        {category.name}
+                                                    </SelectItem>
+                                                )) }
+                                            </SelectContent>
+                                        </Select>
+                                        <FormDescription>
+                                            Select a category for your AI comoanion
+                                        </FormDescription>
+                                    </FormItem>
+                                )}
+                            />
+                        </div>
+                        <div className=" space-y-2 w-full">
+                            <div>
+                                <h3 className=" text-lg font-medium">
+                                     Configuration
+                                </h3>
+                                <p className=" text-sm text-muted-foreground">
+                                    Default instructions for AI behaviour
+                                </p>
+                            </div>
+                            <Separator className=" bg-primary/10"/>
+                        </div>
                 </form>
             </Form>
         </div>
