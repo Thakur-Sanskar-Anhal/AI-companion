6384d828b24dd20da879652b992598b58057f724
Sanskar <thakursanskaranhal@gmail.com>
2023-12-02 21:37:41 +0530

    created chat heading

---
+++ ted-ai/app/(chat)/(routes)/chat/[chatId]/components/client.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { Companion, Message } from "@prisma/client";
+import { ChatHeader } from "./ChatHeader";
+
+interface ChatClientProps {
+    companion: Companion & {
+        messages: Message[],
+        _count: {
+            messages: number;
+        }
+    }
+}
+
+export const ChatClient = ({
+    companion
+}: ChatClientProps) => {
+    return(
+        <div className=" flex slexcol h-full p-4 space-y-2">
+            <ChatHeader companion={companion} />
+        </div>
+    )
+}
