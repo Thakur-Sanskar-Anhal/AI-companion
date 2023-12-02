5cea0dadd507c21613fbd327c5f5f06bdee472b4
Sanskar <thakursanskaranhal@gmail.com>
2023-12-02 22:37:49 +0530

    started chat

--- ted-ai/app/(chat)/(routes)/chat/[chatId]/components/client.tsx
+++ ted-ai/app/(chat)/(routes)/chat/[chatId]/components/client.tsx
@@ -1,7 +1,12 @@
 "use client";
 
+import { useCompletion } from "ai/react"
+
 import { Companion, Message } from "@prisma/client";
 import { ChatHeader } from "./ChatHeader";
+import { useRouter } from "next/navigation";
+import { FormEvent, useState } from "react";
+import { ChatForm } from "@/components/chat-form";
 
 interface ChatClientProps {
     companion: Companion & {
@@ -15,9 +20,54 @@ interface ChatClientProps {
 export const ChatClient = ({
     companion
 }: ChatClientProps) => {
+
+    const router = useRouter();
+    const [ messages, setMessages] = useState<any[]>(companion.messages)
+
+    const {
+        input,
+        isLoading,
+        handleInputChange,
+        handleSubmit,
+        setInput
+    } = useCompletion({
+        api: `/api/chat/${companion.id}`,
+        onFinish(prompt, completion) {
+            const systemMessage = {
+                role: "system",
+                content: completion,
+            }
+
+            setMessages((current) => [...current, systemMessage]);
+            setInput("");
+
+            router.refresh();
+        },
+    });
+
+    const onSubmit = (e: FormEvent<HTMLFormElement>) => {
+        const userMessage = {
+            role: "user",
+            content: input,
+        }
+
+        setMessages((current) => [...current, userMessage]);
+        handleSubmit(e);
+    }
+
     return(
-        <div className=" flex slexcol h-full p-4 space-y-2">
+        <div className=" flex flex-col h-full p-4 space-y-2">
             <ChatHeader companion={companion} />
+            <div>
+                Messages TODO
+            </div>
+            <ChatForm
+                isLoading= {isLoading}
+                input= {input}
+                handleInputChange= {handleInputChange}
+                onSubmit= {onSubmit}
+
+            />
         </div>
     )
 }
